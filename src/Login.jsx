fef610f8bd1bf2e48ec58c41afb5d65c9933a30f
Naveen-HN <hnnaveen93@gmail.com>
2020-02-05 16:04:58 -0500

    Added footer and removed break tags to align

--- src/Login.jsx
+++ src/Login.jsx
@@ -39,66 +39,60 @@ class Login extends Component {
     return (
       <div>
         <nav>Welcome</nav>
-        <form>
+        <form class="form-horizontal">
           <h3 id="head">Please enter your details below:</h3>
-          <label>First Name:</label>
-          <input
-            id="inp"
-            type="textbox"
-            value={this.state.firstName}
-            name="firstName"
-            placeholder="First Name.."
-            onChange={this.handleChange}
-          />
-          <br />
-          <br />
-          <label>Last Name:</label>
-          <input
-            id="inp"
-            type="textbox"
-            value={this.state.lastName}
-            name="lastName"
-            placeholder="Last Name.."
-            onChange={this.handleChange}
-          />
-          <br />
-          <br />
-          <label>Username:</label>
-          <input
-            id="inp"
-            type="textbox"
-            value={this.state.username}
-            name="username"
-            placeholder="Username"
-            onChange={this.handleChange}
-          />
-          <br />
-          <br />
-          <label>Password:</label>
-          <input
-            id="inp"
-            type="password"
-            value={this.state.password}
-            name="password"
-            placeholder="Password"
-            onChange={this.handleChange}
-          />
-          <br />
-          <br />
-          <label>Confirm password:</label>
-          <input
-            id="inp"
-            type="password"
-            value={this.state.confirmPassword}
-            name="confirmPassword"
-            placeholder="Type password again"
-            onChange={this.handleChange}
-          />
-          <br />
-          <br />
+          <div class="container">
+            <label>First Name:</label>
+            <input
+              id="inp"
+              type="textbox"
+              value={this.state.firstName}
+              name="firstName"
+              placeholder="First Name.."
+              onChange={this.handleChange}
+            />
+            <br />
+            <label>Last Name:</label>
+            <input
+              id="inp"
+              type="textbox"
+              value={this.state.lastName}
+              name="lastName"
+              placeholder="Last Name.."
+              onChange={this.handleChange}
+            />
+            <label>Username:</label>
+            <input
+              id="inp"
+              type="textbox"
+              value={this.state.username}
+              name="username"
+              placeholder="Username"
+              onChange={this.handleChange}
+            />
+            <label>Password:</label>
+            <input
+              id="inp"
+              type="password"
+              value={this.state.password}
+              name="password"
+              placeholder="Password"
+              onChange={this.handleChange}
+            />
+            <label>Confirm password:</label>
+            <input
+              id="inp"
+              type="password"
+              value={this.state.confirmPassword}
+              name="confirmPassword"
+              placeholder="Type password again"
+              onChange={this.handleChange}
+            />
+          </div>
           <button onClick={this.handleSubmit}>Submit</button>
           <button onClick={this.handleReset}>Reset</button>
         </form>
+        <nav id="footer"> &copy; Created by Naveen </nav>
       </div>
     );
   }
